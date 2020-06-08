25a2685dcbc8daf4987e9d3fdc6a9dd7f4325927
cristian536 <cdllanosp@libertadores.edu.co>
2020-06-07 23:51:34 -0500

    datos

--- src/component/visual/visual.js
+++ src/component/visual/visual.js
@@ -1,11 +1,12 @@
 import './visual.css';
-
-  
-import React, { useEffect,} from 'react';
-
+import React, { useEffect, useState, } from 'react';
 import axios from 'axios';
+import { TextField } from '@material-ui/core';
 
 function visual() {
+    const useState = props => {
+    const [textValue, setTextValue] = useState('');
+    };
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(()=> {
         axios.get('http://localhost:8080/user').then((response)=> {
@@ -14,10 +15,19 @@ function visual() {
         .catch((error)=> {});
     });
 
-    //const arr =['richard', 'alexander'];
+    const onChangeTextFiled1 = (event) =>{
+        console.log(event.target.value);
+        //setTextValue(event.target.value)/;
+    }
+
+    const arr =['richard', 'alexander'];
     return (
         <div>
             <h1>{'Pagina de visualizacion'}</h1>
+            <TextField onChange = {onChangeTextFiled1}/>
+            {arr.map((val) => ( 
+            <p key = {Math.floor(Math.random() * 1000)}>{val}</p>
+            ))}
         </div>
     );
 }
