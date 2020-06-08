d9e2ad01c8b251def24c11dad3ef8ca7620cfd51
racortazarf <racortazarf@libertadores.edu.co>
2020-06-07 19:51:54 -0500

    se agrega llamado parcial de usuarios

--- src/component/visual/visual.js
+++ src/component/visual/visual.js
@@ -1,13 +1,25 @@
 import './visual.css';
 
-import React from 'react'
+  
+import React, { useEffect,} from 'react';
+
+import axios from 'axios';
 
 function visual() {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    useEffect(()=> {
+        axios.get('http://localhost:8080/user').then((response)=> {
+            console.log(response);
+        })
+        .catch((error)=> {});
+    });
+
+    //const arr =['richard', 'alexander'];
     return (
         <div>
             <h1>{'Pagina de visualizacion'}</h1>
         </div>
-    )
+    );
 }
 
 export default visual;
