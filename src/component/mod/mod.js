45d070bf480183dea5207312c3f21d969b259e57
camilo95951 <camilo.salgado95951@gmail.com>
2020-06-08 23:46:32 -0500

    algunos estilos css y arc de mod

--- src/component/mod/mod.js
+++ src/component/mod/mod.js
@@ -1,13 +1,89 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import './mod.css';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function mod() {
+    // eslint-disable-next-line no-unused-vars
+    const [textValue, setTextValue] = useState('');
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [userData, setUserData] = useState(null);
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [statusFlag, setStatusFlag] = useState(false);
+
+    useEffect(()=> {
+        if (!statusFlag){
+            axios.get('http://localhost:8080/user'
+            ).then((response)=> {
+            console.log(response);
+            setUserData(response);
+            setStatusFlag(true);
+        })
+        .catch((error)=> {});
+        }
+        
+    });
+
+    const mdData = (params) => {
+        // eslint-disable-next-line no-lone-blocks
+        
+        return (
+          <div>
+            <h3>{"Actualización de datos"}</h3>     
+            <br />  
+            <table>
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Nombre</th>
+                  <th>Apellido</th>
+                  <th>Edad</th>
+                  <th>Correo</th>
+                </tr>
+              </thead>
+              <tbody>
+                {userData === null
+                  ? null
+                  : userData.data.map((val) => (
+                      <tr key={val.userid}>
+                        <td>{val.userid}</td>
+                        <td>{val.name}</td>
+                        <td>{val.lastname}</td>
+                        <td>{val.age}</td>
+                        <td>{val.email}</td>
+                      </tr>
+                    ))}
+              </tbody>
+            </table>
+         </div>
+      ); 
+           
+    };
+        
+
+/*
+    //const arr =['richard', 'alexander'];
+    return (
+        <div>
+            <h1>{'Usuarios registrados'}</h1>
+            {userData === null ? null : vsData()}
+        </div>
+    );
+    */
+
+
+
+
+
+
     return (
         <div>
             <h1>{'Pagina de modificacion'}</h1>
+            {userData === null ? null : mdData()}
         </div>
-    )
+    );
+    
 }
 
-export default mod
+export default mod;
